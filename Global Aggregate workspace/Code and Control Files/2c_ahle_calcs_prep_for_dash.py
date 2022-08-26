02f616d657416ab8e1a4900dbb75f5895e3c022b
jdreplogle <jreplogle@firstanalytics.com>
2022-08-26 13:35:39 -0700

    Data updates

--- Global Aggregate workspace/Code and Control Files/2c_ahle_calcs_prep_for_dash.py
+++ Global Aggregate workspace/Code and Control Files/2c_ahle_calcs_prep_for_dash.py
@@ -125,6 +125,8 @@ dropcols = [
 #### Drop problematic rows
 # =============================================================================
 droprows = (world_ahle_abt['country'].str.upper() == 'FRENCH GUIANA')
+droprows = (world_ahle_abt['country'].str.upper() == 'GUADELOUPUE')
+droprows = (world_ahle_abt['country'].str.upper() == 'MARTINIQUE')
 
 world_ahle_abt_fordash = world_ahle_abt.loc[~ droprows].drop(columns=dropcols ,errors='ignore')
 datainfo(world_ahle_abt_fordash)

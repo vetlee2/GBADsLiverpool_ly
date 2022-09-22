5a11d07a13f7378683ef5ee8154bbef9115b9a23
kmccaffrey2 <kmccaffrey@firstanalytics.com>
2022-09-22 11:07:36 -0400

    Small updates

--- AHLE Dashboard/Dash App/gbadsDash.py
+++ AHLE Dashboard/Dash App/gbadsDash.py
@@ -482,7 +482,7 @@ for i in np.sort(ecs_ahle_summary['species'].unique()):
 
 
 # Display
-ecs_display_options = [{'label': i, 'value': i, 'disabled': False} for i in ["Current & Ideal",
+ecs_display_options = [{'label': i, 'value': i, 'disabled': False} for i in ["Compare Current",
                                                                              "Difference (AHLE)",
                                                                             ]]
 
@@ -2632,7 +2632,7 @@ gbadsDash.layout = html.Div([
                             html.H6("Display"),
                             dcc.RadioItems(id='select-display-ecs',
                                           options=ecs_display_options,
-                                          value='Current & Ideal',
+                                          value='Compare Current',
                                           labelStyle={'display': 'block'},
                                           inputStyle={"margin-right": "2px"}, # This pulls the words off of the button
                                           ),
@@ -5062,8 +5062,9 @@ def update_attr_treemap_ecs(input_json, prodsys, age, sex, currency,
     ecs_treemap_fig.update_layout(title_text=f'Attribution | All Small Ruminants <br><sup> Using {prodsys} for {age} and {sex}</sup><br>',
                                   font_size=15,
                                   margin=dict(t=100))
+    # !!! - NEED TO FIX THIS WITH NEW HIERARCHY OPTIONS
     # Add % of total AHLE
-    ecs_treemap_fig.data[0].texttemplate = "%{label}<br>% of Total AHLE=%{customdata[0]:,.2f}%"
+    # ecs_treemap_fig.data[0].texttemplate = "%{label}<br>% of Total AHLE=%{customdata[0]:,.2f}%"
 
     # Add tooltip
     if currency == 'Birr':
@@ -5923,7 +5924,7 @@ def update_ahle_waterfall_ga(input_json ,selected_region ,selected_incgrp ,selec
     total_ahle = prep_df_sums[prep_df_sums['item']=='Net value']['value_usd_ahle_diff'].values[0]
     # total_ahle = total_ahle + prod_vetspend + pub_vetspend
 
-    if display =='Current & Ideal':
+    if display =='Compare Current':
         # Create graph with current values
         name = 'Current'
         measure = ["relative", "relative", "relative", "relative", "relative", "relative", "relative", "total"]
@@ -5946,7 +5947,7 @@ def update_ahle_waterfall_ga(input_json ,selected_region ,selected_incgrp ,selec
             waterfallgroupgap = 0.5,    # Gap between bars
             )
 
-        ga_waterfall_fig.update_layout(title_text=f'Current & ideal output values and costs | {print_selected_country}{print_selected_incgrp}{selected_year}<br><sup>Total animal health loss envelope: ${total_ahle :,.0f} in constant 2010 US dollars</sup><br>',
+        ga_waterfall_fig.update_layout(title_text=f'Compare Current output values and costs | {print_selected_country}{print_selected_incgrp}{selected_year}<br><sup>Total animal health loss envelope: ${total_ahle :,.0f} in constant 2010 US dollars</sup><br>',
                                        yaxis_title='US Dollars (2010 constant)',
                                        font_size=15)
     else:
@@ -6031,7 +6032,7 @@ def update_ahle_lineplot_ga(input_json ,selected_region ,selected_incgrp ,select
     prep_df_sums = prep_df_sums.reset_index()
 
 
-    if display == "Current & Ideal":
+    if display == "Compare Current":
         # Plot current value
         plot_current_value = go.Scatter(
             x=prep_df_sums['year']
